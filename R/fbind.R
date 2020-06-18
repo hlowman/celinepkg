442c7cfa5dfc55e648fb1bdb17de4138974fb6aa
Heili Lowman <heilil@sccwrp.org>
2020-06-18 14:55:01 -0700

    Description and license edits commit.

--- R/fbind.R
+++ R/fbind.R
@@ -7,6 +7,19 @@
 
 # DROP THAT LOGIC into a function. But we can only have the below as code in this .R file since it needs to be universal in application.
 
+#' Bind two factors
+#'
+#' Create a new factor from two existing factors, where the new factor's levels
+#' are the union of the levels of the input factors.
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
