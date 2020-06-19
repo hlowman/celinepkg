f0d338a4a1f16c4d9e1870f76866289b1016f6cf
Heili Lowman <heilil@sccwrp.org>
2020-06-19 14:37:25 -0700

    Testing chapter commit.

--- tests/testthat/test-fbind.R
+++ tests/testthat/test-fbind.R
@@ -7,3 +7,6 @@ test_that("fbind() binds factor (or character)", {
   expect_identical(fbind(x, y), z)
   expect_identical(fbind(x_fact, y), z)
 })
+
+# Expectations have two arguments - first is the actual result, the second is what you expect.
+# When run in the console, true arguments don't yield anything, but false ones yield an error.
